aa022a8ac3d613ea04f355cc180794d695bd454a
neilmrtngamboa <nmg.gamboa@gmail.com>
2024-04-13 14:47:17 +0800

    Added 'onAuthStateChanged' to protect the routes

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,5 +1,28 @@
+import firebaseApp from '../FirebaseConfig/FirebaseConfig.jsx'
+import { getAuth, onAuthStateChanged, signOut } from 'firebase/auth';
+import { useEffect, useState } from 'react';
+import { useNavigate } from "react-router-dom";
+
 
 function Home () {
+
+    let navigate = useNavigate();
+    const auth = getAuth(firebaseApp);
+    const [userProfile, setUserProfile] = useState({})
+
+    useEffect (() => {
+        onAuthStateChanged(auth, (user) => {
+            if (user) {
+                setUserProfile({
+                    email: user.email,
+                    username: user.displayName
+                })
+            } else {
+                navigate('/login');
+            }
+        });
+    }, [])
+
     return (
         <>
         <h1>Home Page</h1>
