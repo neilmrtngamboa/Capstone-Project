82340dc3c8881cc22586651c7207559417d2ca06
neilmrtngamboa <nmg.gamboa@gmail.com>
2024-04-15 17:23:23 +0800

    added a delete button for payment

--- src/Pages/ListOfPayments.jsx
+++ src/Pages/ListOfPayments.jsx
@@ -3,6 +3,7 @@ function ListOfPayments ({name, unit, amount, status, date, paymentID}) {
         <>
         <div className="border-1 border-black">
             <p>{name} {unit} {amount} {status} {date}</p>
+            <button className='border-1 border-black p-1 bg-red-500 hover:bg-red-700'>Delete Payment</button>
 
         </div>
         </>
