c37eaa604a9e5d144407e3dc12f5318972743d58
neilmrtngamboa <nmg.gamboa@gmail.com>
2024-04-16 17:32:45 +0800

    modified the functions, values and the onchange

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -84,7 +84,7 @@ function Home () {
     }
 
     const setUpdate = (tenantID,firstname,lastname,unit) => {
-        setEditTenant({
+        setTenant({
             tenantID: tenantID,
             firstname: firstname,
             lastname: lastname,
@@ -95,13 +95,13 @@ function Home () {
 
     const updateTenantDetails = () => {
 
-        updateDoc(doc(db,'tenants',editTenant.tenantID), { 
-            firstname: editTenant.firstname,
-            lastname: editTenant.lastname,
-            unit: editTenant.unit,
+        updateDoc(doc(db,'tenants',tenant.tenantID), { 
+            firstname: tenant.firstname,
+            lastname: tenant.lastname,
+            unit: tenant.unit,
           })
 
-          setEditTenant({
+          setTenant({
             firstname: '',
             lastname: '',
             unit: ''
@@ -116,13 +116,13 @@ function Home () {
         <hr />
         <h5>Add a new tenant</h5>
         <input type="text" placeholder='First Name' 
-        onChange={(e) => setEditTenant({...editTenant, firstname: e.target.value})} value={editTenant.firstname}
+        onChange={(e) => setTenant({...tenant, firstname: e.target.value})} value={tenant.firstname}
         />
         <input type="text" placeholder='Last Name'
-        onChange={(e) => setEditTenant({...editTenant, lastname: e.target.value})} value={editTenant.lastname}
+        onChange={(e) => setTenant({...tenant, lastname: e.target.value})} value={tenant.lastname}
         />
         <input type="text" placeholder='Unit'
-        onChange={(e) => setEditTenant({...editTenant, unit: e.target.value})} value={editTenant.unit}
+        onChange={(e) => setTenant({...tenant, unit: e.target.value})} value={tenant.unit}
         />
         <button className='border-2 border-black p-2 bg-blue-500 hover:bg-blue-700' onClick={addTenant}>Add+</button>
         <button className='border-2 border-black p-2 bg-yellow-500 hover:bg-yellow-700' onClick={updateTenantDetails}>Update</button>
