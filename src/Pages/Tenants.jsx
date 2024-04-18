0ad2babecb4adca087f54e24485fd244f7ae49e8
neilmrtngamboa <nmg.gamboa@gmail.com>
2024-04-18 21:26:16 +0800

    added margin on the delete button

--- src/Pages/Tenants.jsx
+++ src/Pages/Tenants.jsx
@@ -29,9 +29,9 @@ function Tenants({ firstname, lastname, unit, deleteTenant, tenantID, setUpdate,
                             <h5 className="text-lg font-light">{firstname} {lastname}</h5>
                             <h5 className="text-lg">Unit: <b>{unit}</b></h5>
 
-                            <div className="grid grid-cols-2 gap-5 mt-3">
+                            <div className="mt-3">
                                 <button className='rounded shadow-lg bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 border-b-4 
-                                border-red-600 hover:border-red-800'
+                                border-red-600 hover:border-red-800 me-3'
                                     onClick={() => deleteTenant(tenantID)}>Delete</button>
                                 <button className='rounded shadow-lg bg-emerald-500 hover:bg-emerald-600 text-white font-bold py-2 px-4 border-b-4 
                                 border-emerald-600 hover:border-emerald-800'
