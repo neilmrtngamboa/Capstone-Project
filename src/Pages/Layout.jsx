6b5a3eaf69289f05ca7c50487a2f8f7dc6b369fd
neilmrtngamboa <nmg.gamboa@gmail.com>
2024-04-19 14:21:43 +0800

    hide tha navbar links based on the authentication status

--- src/Pages/Layout.jsx
+++ src/Pages/Layout.jsx
@@ -1,17 +1,19 @@
 import { Outlet, Link, useNavigate } from "react-router-dom";
 import firebaseApp from '../FirebaseConfig/FirebaseConfig.jsx';
 import {getAuth, onAuthStateChanged, signOut} from "firebase/auth";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function Layout () {
 
     let navigate = useNavigate();
     const auth = getAuth(firebaseApp);
+    const [authenticated, setAuthentication] = useState(false)
 
     useEffect(() => {
 
         onAuthStateChanged(auth, (user) => {
             if (user) {
+                setAuthentication(true)
                 const uid = user.uid;
             } else {
                 navigate('/login');
@@ -24,6 +26,7 @@ function Layout () {
         signOut(auth).then(() => {
             navigate('/login');
         })
+        setAuthentication(false)
 
     }
 
@@ -31,25 +34,39 @@ function Layout () {
         <main className="bg-indigo-50">
 
             <nav class="flex items-center justify-between flex-wrap bg-indigo-500 p-6">
+
                 <div class="flex items-center flex-shrink-0 text-white mr-6">
                     <span class="font-semibold text-xl tracking-tight">Apartment Management System</span>
                 </div>
             
                 <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+
                     <div class="text-sm lg:flex-grow">
-                    <Link to='/' class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-violet-800 mr-4">
+                    {
+                        authenticated ?
+                        <>
+                        <Link to='/' class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-violet-800 mr-4">
                         Tenant Dashboard
-                    </Link>
-                    <Link to='/payments' class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-violet-800 mr-4">
-                        Payments
-                    </Link>
+                        </Link>
+                        <Link to='/payments' class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-violet-800 mr-4">
+                            Payments
+                        </Link>
+                        </>
+                        :
+                        <></>
+                    }
                     </div>
 
-                    <div>
-                    <button onClick={Logout} class="inline-block text-sm px-4 py-2 leading-none border rounded 
-                    text-white border-white hover:border-transparent hover:text-violet-800 hover:bg-white 
-                    mt-4 lg:mt-0">Log out</button>
-                    </div>
+                    {
+                        authenticated ? 
+                        <div>
+                            <button onClick={Logout} class="inline-block text-sm px-4 py-2 leading-none border rounded 
+                            text-white border-white hover:border-transparent hover:text-violet-800 hover:bg-white 
+                            mt-4 lg:mt-0">Log out</button>
+                        </div>
+                        :
+                        <></>
+                    }
 
                 </div>
             </nav>
