3c382ca1b880b5a0770ff90024139f442f470089
neilmrtngamboa <nmg.gamboa@gmail.com>
2024-04-19 14:30:49 +0800

    added a ternary operator on the footer

--- src/Pages/Layout.jsx
+++ src/Pages/Layout.jsx
@@ -79,20 +79,25 @@ function Layout () {
 
         <footer class="bg-transparent mt-60">
             <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
-                <div class="sm:flex sm:items-center sm:justify-between">
-                    <Link to='/' class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse hover:text-violet-900">
-                        <span class="self-center lg:text-2xl sm:text-xl font-semibold whitespace-nowrap text-gray-400">
-                        Apartment Management System</span>
-                    </Link>
-                    <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-400 sm:mb-0">
-                        <li>
-                            <a href="https://github.com/neilmrtngamboa" class="hover:underline me-4 md:me-6">GitHub</a>
-                        </li>
-                        <li>
-                            <a href="https://www.linkedin.com/in/neilmartingamboa/" class="hover:underline me-4 md:me-6">LinkedIn</a>
-                        </li>
-                    </ul>
-                </div>
+                {
+                    authenticated ? 
+                    <div class="sm:flex sm:items-center sm:justify-between">
+                        <Link to='/' class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse hover:text-violet-900">
+                            <span class="self-center lg:text-2xl sm:text-xl font-semibold whitespace-nowrap text-gray-400">
+                            Apartment Management System</span>
+                        </Link>
+                        <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-400 sm:mb-0">
+                            <li>
+                                <a href="https://github.com/neilmrtngamboa" class="hover:underline me-4 md:me-6">GitHub</a>
+                            </li>
+                            <li>
+                                <a href="https://www.linkedin.com/in/neilmartingamboa/" class="hover:underline me-4 md:me-6">LinkedIn</a>
+                            </li>
+                        </ul>
+                    </div>
+                : 
+                <></>
+                }
                 <hr class="my-6 border-slate-200 sm:mx-auto lg:my-8" />
                 <span class="block text-sm text-gray-500 sm:text-center ">© 2024. Developed by: Neil Martin Gamboa</span>
             </div>
