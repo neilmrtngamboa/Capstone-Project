6ba8848966cd4ea8387c9135e1e2cef8f7a58151
neilmrtngamboa <nmg.gamboa@gmail.com>
2024-04-19 18:44:11 +0800

    imported the necessary

--- src/Pages/MovedTenants.jsx
+++ src/Pages/MovedTenants.jsx
@@ -1,3 +1,8 @@
+import firebaseApp from '../FirebaseConfig/FirebaseConfig.jsx'
+import { getAuth, onAuthStateChanged, signOut } from 'firebase/auth';
+import { getFirestore, collection, addDoc, onSnapshot, deleteDoc, doc, updateDoc, Timestamp } from 'firebase/firestore'
+import { useEffect, useState } from 'react';
+import { useNavigate, Link } from "react-router-dom";
 
 function MovedTenants () {
     return (
