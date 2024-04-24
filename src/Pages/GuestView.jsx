40c2581271299e48ad93cc438d55d9311acc1b9d
neilmrtngamboa <nmg.gamboa@gmail.com>
2024-04-24 09:16:22 +0800

    added the necessary imports

--- src/Pages/GuestView.jsx
+++ src/Pages/GuestView.jsx
@@ -1,12 +1,14 @@
-
+import { useEffect, useState } from 'react';
+import { getFirestore, collection, onSnapshot} from "firebase/firestore";
+import firebaseApp from '../FirebaseConfig/FirebaseConfig.jsx'
 
 
 function GuestView() {
     
     return (
-        <>
+        <div className="mt-60">
         <h5>This is the guest page</h5>
-        </>
+        </div>
         )
 }
 
