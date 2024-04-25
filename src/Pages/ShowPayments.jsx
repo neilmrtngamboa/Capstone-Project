119c71519cfacd53967f2331438c341342898467
neilmrtngamboa <nmg.gamboa@gmail.com>
2024-04-25 12:54:51 +0800

    created a new component to show payments

---
+++ src/Pages/ShowPayments.jsx
@@ -0,0 +1,11 @@
+import { useEffect, useState } from 'react';
+import { getFirestore, collection, onSnapshot} from "firebase/firestore";
+import firebaseApp from '../FirebaseConfig/FirebaseConfig.jsx'
+
+function ShowPayments() {
+
+    
+
+}
+
+export default ShowPayments;
