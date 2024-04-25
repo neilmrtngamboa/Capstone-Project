5944902a7ef3177e6501f858d1aabc30dfa8c6b2
neilmrtngamboa <nmg.gamboa@gmail.com>
2024-04-25 13:01:55 +0800

    added authentication

--- src/Pages/ShowPayments.jsx
+++ src/Pages/ShowPayments.jsx
@@ -1,13 +1,26 @@
 import { useEffect, useState } from 'react';
 import { getFirestore, collection, onSnapshot} from "firebase/firestore";
 import firebaseApp from '../FirebaseConfig/FirebaseConfig.jsx'
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
 
 function ShowPayments() {
 
     const db = getFirestore(firebaseApp)
+    const auth = getAuth(firebaseApp);
     const [payments, setPayments] = useState([])
+    const [userProfile, setUserProfile] = useState({})
     
     useEffect (() => {
+
+        onAuthStateChanged(auth, (user) => {                //Authentication
+            if (user) {
+                setUserProfile({
+                    email: user.email,                      
+                })
+            } else {
+                navigate('/login');
+            }
+        });
 
         onSnapshot(collection(db, 'payments'), getPayments => {            //use firebase function onSnapshot to fetch data from the database and collection
             const newPayments = []                         
