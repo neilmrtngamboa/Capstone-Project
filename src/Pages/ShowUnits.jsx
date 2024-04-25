91ac5ada4a514b3b8c052255bc925b4437d5e254
neilmrtngamboa <nmg.gamboa@gmail.com>
2024-04-25 15:13:05 +0800

    created a new component

---
+++ src/Pages/ShowUnits.jsx
@@ -0,0 +1,10 @@
+import { useEffect, useState } from 'react';
+import { getFirestore, collection, onSnapshot } from "firebase/firestore";
+import firebaseApp from '../FirebaseConfig/FirebaseConfig.jsx'
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+
+function ShowUnits () {
+
+}
+
+export default ShowUnits;
