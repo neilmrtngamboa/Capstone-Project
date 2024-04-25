33cfe8203ea39251aced61ba94dc6e503499dbb8
neilmrtngamboa <nmg.gamboa@gmail.com>
2024-04-25 12:59:48 +0800

    created a useEffect to show data

--- src/Pages/ShowPayments.jsx
+++ src/Pages/ShowPayments.jsx
@@ -4,6 +4,21 @@ import firebaseApp from '../FirebaseConfig/FirebaseConfig.jsx'
 
 function ShowPayments() {
 
+    const db = getFirestore(firebaseApp)
+    const [payments, setPayments] = useState([])
+    
+    useEffect (() => {
+
+        onSnapshot(collection(db, 'payments'), getPayments => {            //use firebase function onSnapshot to fetch data from the database and collection
+            const newPayments = []                         
+            getPayments.forEach(payment => {
+                newPayments.push(payment.data())                   //push the fetched data into the array
+                setPayments(newPayments)
+            })
+        }
+        )
+    }, [])
+
     
 
 }
