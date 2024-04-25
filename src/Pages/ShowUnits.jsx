4d03054cfa444cfe600da273971dc8fece8eacd3
neilmrtngamboa <nmg.gamboa@gmail.com>
2024-04-25 15:52:18 +0800

    added a condition and return value

--- src/Pages/ShowUnits.jsx
+++ src/Pages/ShowUnits.jsx
@@ -23,7 +23,7 @@ function ShowUnits () {
             }
         });
 
-        onSnapshot(collection(db, 'payments'), fetchData => {
+        onSnapshot(collection(db, 'tenants'), fetchData => {
             const newUnits = []
             fetchData.forEach(unit => {
                 newUnits.push(unit.data())
@@ -33,6 +33,36 @@ function ShowUnits () {
         )
 
     }, [])
+
+    if (units.length > 0) {
+        return (
+            <main className='p-10 grid lg:grid-cols-5 lg:gap-5'>
+                {
+                    units.map(getUnits => 
+                        <div className='border-2 border-slate-400 p-5 rounded shadow-md mt-5 grid lg:grid-cols-2'>
+                            <img src="https://icons.iconarchive.com/icons/aha-soft/security/256/key-icon.png" className='h-14 w-14 my-auto' alt="" />
+                            
+                            <div>
+                            <h3 className='font-light'>Unit: <b className='font-bold'>{getUnits.unit}</b> </h3>
+                            <h3 className='font-light'>Contact No: <b className='font-bold'>{getUnits.phonenumber}</b></h3>
+                            <h3 className='font-light'>Date of Arrival: <b className='font-bold'>{getUnits.date.toDate().toLocaleTimeString('en-US', { month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit', year: '2-digit' })}</b></h3>
+                            </div>
+                            
+                                                  
+                            
+                        </div>
+                        )
+                }
+
+            </main>
+        )
+    } else {
+        return (
+            <main className='p-20 mt-5'>
+                <h1 className='flex justify-center mt-10 font-bold text-2xl text-gray-400'>There are no tenants available.</h1>
+            </main>
+        )
+    }
 
 }
 
