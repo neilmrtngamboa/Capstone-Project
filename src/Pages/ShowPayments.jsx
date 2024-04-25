bc2a3e08a399c78e3ae0933ef07f9abde2a8283d
neilmrtngamboa <nmg.gamboa@gmail.com>
2024-04-25 13:17:57 +0800

    added a print button

--- src/Pages/ShowPayments.jsx
+++ src/Pages/ShowPayments.jsx
@@ -41,20 +41,25 @@ function ShowPayments() {
                 {
                     payments.map(showPayments =>
                         <>
+                            <button className="flex mx-auto mt-3 px-4 py-2 border-2 bg-white hover:bg-gray-100 text-gray-800 font-semibold rounded shadow"
+                                onClick={() => window.print()}
+                            >
+                                Print payments
+                            </button>
                             <div className="border-2 border-slate-400 p-5 rounded shadow-md mt-5">
                                 <h4 className="font-light">Name: <b className="font-semibold me-1">{showPayments.name}</b>
                                     Unit: <b className="font-semibold">{showPayments.unit}</b> Amount: <b className="font-semibold me-1">{showPayments.amount}</b>
-                                    Status: <b className="font-semibold">{showPayments.status}</b> Date: <b className="font-semibold">{showPayments.date.toDate().toLocaleTimeString('en-US', { month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit', year: '2-digit' })}</b></h4>  
+                                    Status: <b className="font-semibold">{showPayments.status}</b> Date: <b className="font-semibold">{showPayments.date.toDate().toLocaleTimeString('en-US', { month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit', year: '2-digit' })}</b></h4>
                             </div>
                         </>
                     )
                 }
             </main>
         )
-    } else{
-        return(
+    } else {
+        return (
             <main className='p-20 mt-5'>
-            <h1 className='flex justify-center mt-10 font-bold text-2xl text-gray-400'>There are no payments to show.</h1>
+                <h1 className='flex justify-center mt-10 font-bold text-2xl text-gray-400'>There are no payments to show.</h1>
             </main>
         )
     }
