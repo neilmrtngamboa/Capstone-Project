8e5ee5d4557e52e1c7aff55f1c8f97b38d426941
neilmrtngamboa <nmg.gamboa@gmail.com>
2024-04-25 15:17:59 +0800

    added authentication to the ShowUnits page

--- src/Pages/ShowUnits.jsx
+++ src/Pages/ShowUnits.jsx
@@ -4,6 +4,26 @@ import firebaseApp from '../FirebaseConfig/FirebaseConfig.jsx'
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
 
 function ShowUnits () {
+    
+    const db = getFirestore(firebaseApp)
+    const auth = getAuth(firebaseApp)
+
+    const [units, setUnits] = useState([])
+    const [userProfile, setUserProfile] = useState({})
+
+    useEffect(() => {
+
+        onAuthStateChanged(auth, (user) => {                //Authentication
+            if (user) {
+                setUserProfile({
+                    email: user.email,
+                })
+            } else {
+                navigate('/login');
+            }
+        });
+
+    }, [])
 
 }
 
